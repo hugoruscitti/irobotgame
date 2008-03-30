b188ca1e96b573a35e082db99dce8ddf022e1e50
hugoruscitti <hugoruscitti@e4552e70-a249-0410-9c2d-5b8055106b9a>
2008-03-30 19:00:41 +0000

    Reescalado de la ventana y fondo de la escena juego.

--- trunk/lib/game.py
+++ trunk/lib/game.py
@@ -1,12 +1,15 @@
 # -*- encoding: utf-8 -*-
 from scene import Scene
+import common
 
 class Game(Scene):
     "Escena de juego donde los personajes están en el escenario."
 
     def __init__(self, world):
         Scene.__init__(self, world)
+        self._background = common.load_image('game_background.png')
 
     def on_draw(self):
-        print "orint"
-        pass
+        self.world.clear()            # FIXME: evitar usar este clear por los bordes
+                                      # de pantalla
+        self._background.blit(0, 0)
--- trunk/lib/world.py
+++ trunk/lib/world.py
@@ -11,9 +11,13 @@ class World(pyglet.window.Window):
         pyglet.window.Window.__init__(self, caption='I Robot ?', resizable=True) 
         self._scene = None
         self.change_scene(game.Game(self))
+        self._init_window_size()
 
         if config.DEBUG:
             self.set_location(400, 400)
+            self.fps = pyglet.clock.ClockDisplay()
+        else:
+            self.fps = None
 
     def run(self):
         pyglet.app.run()
@@ -24,3 +28,72 @@ class World(pyglet.window.Window):
 
         self._scene = scene
         self.push_handlers(scene)
+
+    def on_draw(self):
+        self.fps.draw()
+
+    # TODO: Llevar estas rutinas a otra clase abstracta, o extender "cocos.director".
+    def _init_window_size(self):
+        self._window_original_width = self.width
+        self._window_original_height = self.height
+        self._window_aspect =  self.width / float( self.height )
+        self._offset_x = 0
+        self._offset_y = 0
+
+    def get_virtual_coordinates( self, x, y ):
+        """Transforms coordinates that belongs the *real* window size, to the
+        coordinates that belongs to the *virtual* window.
+
+        For example, if you created a window of 640x480, and it was resized
+        to 640x1000, then if you move your mouse over that window,
+        it will return the coordinates that belongs to the newly resized window.
+        Probably you are not interested in those coordinates, but in the coordinates
+        that belongs to your *virtual* window. 
+
+        :rtype: (x,y)           
+        :returns: Transformed coordinates from the *real* window to the *virtual* window
+        """
+
+        x_diff = self._window_original_width / float( self.window.width - self._offset_x * 2 )
+        y_diff = self._window_original_height / float( self.window.height - self._offset_y * 2 )
+
+        adjust_x = (self.window.width * x_diff - self._window_original_width ) / 2
+        adjust_y = (self.window.height * y_diff - self._window_original_height ) / 2
+
+        return ( int( x_diff * x) - adjust_x,   int( y_diff * y ) - adjust_y )
+
+
+    def on_resize( self, width, height):
+        """Method that is called every time the main window is resized.
+        
+        :Parameters:
+            `width` : Integer
+                New width
+            `height` : Integer
+                New height
+        """
+        width_aspect = width
+        height_aspect = int( width / self._window_aspect)
+
+        if height_aspect > height:
+            width_aspect = int( height * self._window_aspect )
+            height_aspect = height
+
+        self._offset_x = (width - width_aspect) / 2
+        self._offset_y =  (height - height_aspect) / 2
+
+        glViewport(self._offset_x, self._offset_y, width_aspect, height_aspect )
+        glMatrixMode(gl.GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0, self._window_original_width, 0, self._window_original_height, -1, 1)
+        glMatrixMode(gl.GL_MODELVIEW)
+
+        
+    #
+    # Misc functions
+    #
+    def enable_alpha_blending( self ):
+        """Enables alpha blending in OpenGL using the GL_ONE_MINUS_SRC_ALPHA algorithm."""
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
